13535ff51f2c2dff24ba96a2d2a01ec3e3fdc7dc
Ruslan <rus.ilin1979@gmail.com>
2023-06-17 22:55:18 +0500

    first commit autocomplete

--- html-forms/autocomplete/task.js
+++ html-forms/autocomplete/task.js
@@ -1,19 +1,19 @@
 class Autocomplete {
-  constructor( container ) {
+  constructor(container) {
     this.container = container;
-    this.input = container.querySelector( '.autocomplete__input' );
-    this.searchInput = container.querySelector( '.autocomplete__search' );
-    this.list = container.querySelector( '.autocomplete__list' );
-    this.valueContainer = container.querySelector( '.autocomplete__value' );
-    this.valueElement = container.querySelector( '.autocomplete__text-content' );
+    this.input = container.querySelector('.autocomplete__input');
+    this.searchInput = container.querySelector('.autocomplete__search');
+    this.list = container.querySelector('.autocomplete__list');
+    this.valueContainer = container.querySelector('.autocomplete__value');
+    this.valueElement = container.querySelector('.autocomplete__text-content');
 
     this.registerEvents();
   }
 
   registerEvents() {
-    this.valueContainer.addEventListener( 'click', e => {
-      this.searchInput.classList.add( 'autocomplete__search_active' );
-      this.list.classList.add( 'autocomplete__list_active' );
+    this.valueContainer.addEventListener('click', e => {
+      this.searchInput.classList.add('autocomplete__search_active');
+      this.list.classList.add('autocomplete__list_active');
       this.searchInput.value = this.valueElement.textContent.trim();
       this.searchInput.focus();
 
@@ -21,11 +21,11 @@ class Autocomplete {
     });
 
 
-    this.searchInput.addEventListener( 'input', e => this.onSearch());
+    this.searchInput.addEventListener('input', e => this.onSearch());
 
-    this.list.addEventListener( 'click', e => {
+    this.list.addEventListener('click', e => {
       const { target } = e;
-      if ( !target.matches( '.autocomplete__item' )) {
+      if (!target.matches('.autocomplete__item')) {
         return;
       }
 
@@ -40,22 +40,22 @@ class Autocomplete {
     });
   }
 
-  onSelect( item ) {
+  onSelect(item) {
     this.input.selectedIndex = item.index;
     this.valueElement.textContent = item.text;
 
-    this.searchInput.classList.remove( 'autocomplete__search_active' );
-    this.list.classList.remove( 'autocomplete__list_active' );
+    this.searchInput.classList.remove('autocomplete__search_active');
+    this.list.classList.remove('autocomplete__list_active');
   }
 
   onSearch() {
-    const matches = this.getMatches( this.searchInput.value );
+    const matches = this.getMatches(this.searchInput.value);
 
-    this.renderMatches( matches );
+    this.renderMatches(matches);
   }
 
-  renderMatches( matches ) {
-    const html = matches.map( item => `
+  renderMatches(matches) {
+    const html = matches.map(item => `
     	<li>
         <span class="autocomplete__item"
         	data-index="${item.index}"
@@ -67,7 +67,8 @@ class Autocomplete {
     this.list.innerHTML = html.join('');
   }
 
-  getMatches( text ) {
+  getMatches(text) {
+
     /*
       TODO: этот метод нужно дописать
       text - фраза, которую вводят в поле поиска
@@ -81,13 +82,25 @@ class Autocomplete {
         value: 'Содержимое атрибута value'
       }
     */
-    return [
+    /* return [
       {
         text: 'Чубакка',
         value: '1'
       }
-    ];
+    ]; */
+    const optionsList = this.input.options;
+    const matches = [];
+
+    for (let i = 0; i < optionsList.length; i++) {
+      if (optionsList[i].text.includes(text)) {
+        matches.push({
+          text: optionsList[i].text,
+          value: optionsList[i].value
+        });
+      }
+    }
+    return matches;
   }
 }
 
-new Autocomplete( document.querySelector( '.autocomplete' ));
+new Autocomplete(document.querySelector('.autocomplete'));
